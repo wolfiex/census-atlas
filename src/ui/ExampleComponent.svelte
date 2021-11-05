5b2029058c616073e0aba18b907974d365234338
Michał Ordon <michal@tehidea.com>
2021-11-05 16:29:57 +0000

    Add ExampleComponent

---
+++ src/ui/ExampleComponent.svelte
@@ -0,0 +1,18 @@
+<script>
+    import ONSButton from "./ons/ONSButton.svelte"
+/**
+ * Example Component props
+ */
+export let text;
+</script>
+
+<div class="example-component">
+    {text}
+    <ONSButton>Click me</ONSButton>
+</div>
+
+<style>
+    .example-component {
+        background-color: aqua;
+    }
+</style>
