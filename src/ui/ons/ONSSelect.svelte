6bca91bd5009f57106eba1adaf46082b0c0bd576
Michał Ordon <michal@tehidea.com>
2021-11-08 21:20:00 +0000

    Fix ONSSelect

--- src/ui/ons/ONSSelect.svelte
+++ src/ui/ons/ONSSelect.svelte
@@ -1,11 +1,13 @@
 <script>
+    export let label;
+    export let id;
+    export let name;
     export let options = [];
 </script>
 
 <div class="ons-field">
-    <label class="ons-label  " for="select">Question topic
-    </label>
-    <select id="select" name="select" class="ons-input ons-input--select">
+    <label class="ons-label " for="{name}">{label}</label>
+    <select id="{id}" name="{name}" class="ons-input ons-input--select">
         {#each options as option}
             <option value={option.value} selected={option.selected} disabled={option.disabled}>{option.label}</option>
         {/each}
