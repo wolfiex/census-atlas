46471320592115d8ba8e2af48aace5681dd1148a
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-11-10 08:46:34 +0000

    changes h1 to h2 to maintain heading structure

--- src/ui/ExploreByTopic.svelte
+++ src/ui/ExploreByTopic.svelte
@@ -2,7 +2,7 @@
     import ONSCollapsible from "./ons/ONSCollapsible.svelte"
 </script>
 
-<h1>Explore by topic</h1>
+<h2>Explore by topic</h2>
 <p>The 2021 Census tells us a lot about how people in England and Wales live and work.</p>
 <p><a>Choose a data option</a></p>
 <ONSCollapsible title="Show me suggestions"></ONSCollapsible>
