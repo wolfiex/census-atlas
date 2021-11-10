303acdfbbd36109a674b35e8497aee1486cb54aa
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-11-10 13:18:52 +0000

    sorts mobile responsiveness

--- src/ui/CategorySelector.svelte
+++ src/ui/CategorySelector.svelte
@@ -1,8 +1,8 @@
-<div class="improve-this-page__prompt ons-grid--flex ons-grid--vertical-center">
+<div class="category-selector ons-grid--flex ons-grid--vertical-center">
   <div class="ons-grid__col ons-col-4@m selector-col ">
     <div class="ons-pl-grid-col ons-grid--flex" style="justify-content:left">
       <span>&#60;&#160;</span>
-      <a class="improve-this-page__page-is-useful-button" href="#"> Very good </a>
+      <a class="category-selector__button" href="#"> Very good </a>
     </div>
   </div>
   <div class="ons-grid__col ons-col-4@m selector-col ons-grid--flex">
@@ -12,22 +12,27 @@
   </div>
   <div class="ons-grid__col ons-col-4@m selector-col">
     <div class="ons-pl-grid-col ons-grid--flex" style="justify-content:right">
-      <a class="improve-this-page__page-is-useful-button" href="#"> Fair </a>
+      <a class="category-selector__button" href="#"> Fair </a>
       <span>&#160;&#62;</span>
     </div>
   </div>
 </div>
 
 <style>
-  .improve-this-page__prompt {
+  .category-selector {
     background-color: #003c57;
     color: #fff;
     padding: 10px 15px 10px;
   }
-  .improve-this-page__prompt a {
+  .category-selector a {
     color: #fff;
   }
-  .improve-this-page__page-is-useful-button {
+  @media (max-width: 400px) {
+      a, span {
+          font-size: 0.8em;
+      }
+  }
+  .category-selector__button {
     margin-right: 0.2em;
   }
   a {
@@ -44,5 +49,10 @@
       margin-bottom: 0;
       font-size: 1.3em;
       font-weight: bold;
+  }
+  @media (max-width: 400px) {
+      p {
+          font-size: 1em;
+      }
   }
 </style>
