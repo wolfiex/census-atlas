2b62d9b4d7b96f72b4547c06433822b93b40155b
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-11-10 15:31:33 +0000

    adds header text to census atlas v2 page

--- src/routes/CensusAtlasV2.svelte
+++ src/routes/CensusAtlasV2.svelte
@@ -1,12 +1,14 @@
 <script>
   import BasePage from "../ui/BasePage.svelte";
+  let serviceTitle = "Explore Census";
+  let description = "Here’s a place where we tell users what the Census Atlas is and what it can do for them";
 </script>
 
 <svelte:head>
   <script defer src="/build/ons-design-system.js"></script>
 </svelte:head>
 
-<BasePage>
+<BasePage {serviceTitle} {description}>
   <h1>Census Atlas ALPHA</h1>
 </BasePage>
 
