6f602f653daf9ca8b80ee9a8683688ae6341ffe0
Michał Ordon <michal@tehidea.com>
2021-11-10 15:04:34 +0000

    Fix build

--- src/routes/DesignSystemApp.svelte
+++ src/routes/DesignSystemApp.svelte
@@ -23,7 +23,7 @@
   import ExploreByTopic from "../ui/ExploreByTopic.svelte";
   import Feedback from "../ui/Feedback.svelte";
   import ExploreByAreaComponent from "../ui/ExploreByAreaComponent.svelte";
-  import ONSExternalHeaderWithDescription from "../ui/ons/ONSExternalHeaderWithDescription.svelte"
+  import ONSExternalHeaderWithDescription from "../ui/ons/ONSExternalHeaderWithDescription.svelte";
   import DesignSystemCode from "../ui/DesignSystemCode.svelte";
   import DesignSystemPanel from "../ui/DesignSystemPanel.svelte";
   let hint = "This is a hint";
@@ -44,7 +44,7 @@
     {
       categoryTitle: "Health",
       subcategory: [
-        {subcategoryTitle: "General health", subcategoryList: ["Very good", "Good", "Fair", "Bad", "Very bad"]},
+        { subcategoryTitle: "General health", subcategoryList: ["Very good", "Good", "Fair", "Bad", "Very bad"] },
         {
           subcategoryTitle: "Effect of long-term ill health...",
           subcategoryList: ["Not limited", "Limited a little", "Limited a lot"],
@@ -54,15 +54,14 @@
   ];
 
   let topicList1 = [
-    {title: "How does general health differ across England and Wales?", href: "#"},
-    {title: "Which areas are home to peoplle who provide the most unpaid care?", href: "#"},
-    {title: "What can we learn about long-term health issues and/or disability?", href: "#"},
+    { title: "How does general health differ across England and Wales?", href: "#" },
+    { title: "Which areas are home to people who provide the most unpaid care?", href: "#" },
+    { title: "What can we learn about long-term health issues and/or disability?", href: "#" },
   ];
 
   let topicList2 = [{ title: "Get Census datasests", href: "#" }];
   let serviceTitle = "Design System";
   let serviceDescription = "Components implemented with Svelte";
-  let topicList2 = [{title: "Get Census datasests", href: "#"}];
 </script>
 
 <svelte:head>
@@ -70,15 +69,13 @@
 </svelte:head>
 
 <ONSSkipLink />
-<ONSExternalHeaderWithDescription serviceTitle={serviceTitle} description={serviceDescription}/>
-<ONSSkipLink/>
-<ONSHeader/>
+<ONSExternalHeaderWithDescription {serviceTitle} description={serviceDescription} />
+<ONSSkipLink />
+<ONSHeader />
 
 <ONSCensusApp>
-  <DesignSystemPanel
-    title="Backlink"
-    code={`<ONSBacklink href="/home"/>`}>
-    <ONSBacklink href="/home"/>
+  <DesignSystemPanel title="Backlink" code={`<ONSBacklink href="/home"/>`}>
+    <ONSBacklink href="/home" />
   </DesignSystemPanel>
 
   <DesignSystemPanel
@@ -86,7 +83,8 @@
     code={`<ONSBreadcrumbs>
   <ONSBreadcrumb href="/home">Home</ONSBreadcrumb>
   <ONSBreadcrumb href="/home/layer-1">Layer 1</ONSBreadcrumb>
-</ONSBreadcrumbs>`}>
+</ONSBreadcrumbs>`}
+  >
     <ONSBreadcrumbs>
       <ONSBreadcrumb href="/home">Home</ONSBreadcrumb>
       <ONSBreadcrumb href="/home/layer-1">Layer 1</ONSBreadcrumb>
@@ -109,8 +107,8 @@
   </p>
   <p>
     <ONSButton disabled>Disabled</ONSButton>
-  </p>`}>
-
+  </p>`}
+  >
     <p>
       <ONSButton onClick={() => alert("Custom on click handler")}>Custom onclick handler</ONSButton>
     </p>
@@ -128,9 +126,7 @@
     </p>
   </DesignSystemPanel>
 
-  <DesignSystemPanel
-    title="Card"
-    code={`<ONSCard title="ONS Card" href="/home">Card</ONSCard>`}>
+  <DesignSystemPanel title="Card" code={`<ONSCard title="ONS Card" href="/home">Card</ONSCard>`}>
     <ONSCard title="ONS Card" href="/home">Card</ONSCard>
   </DesignSystemPanel>
 
@@ -140,7 +136,8 @@
   <ONSCheckbox id="mobile">Mobile</ONSCheckbox>
   <ONSCheckbox id="laptop">Laptop</ONSCheckbox>
   <ONSCheckbox id="tablet">Tablet</ONSCheckbox>
-</ONSCheckboxes>`}>
+</ONSCheckboxes>`}
+  >
     <ONSCheckboxes>
       <ONSCheckbox id="mobile">Mobile</ONSCheckbox>
       <ONSCheckbox id="laptop">Laptop</ONSCheckbox>
@@ -152,15 +149,14 @@
     title="Collapsible"
     code={`<ONSCollapsible title="Collapsible">
   <span>here is the collapsible content</span>
-</ONSCollapsible>`}>
+</ONSCollapsible>`}
+  >
     <ONSCollapsible title="Collapsible">
       <span>here is the collapsible content</span>
     </ONSCollapsible>
   </DesignSystemPanel>
 
-  <DesignSystemPanel
-    title="Input"
-    code={`<ONSTextField id="text-field">Text field</ONSTextField>`}>
+  <DesignSystemPanel title="Input" code={`<ONSTextField id="text-field">Text field</ONSTextField>`}>
     <ONSTextField id="text-field">Text field</ONSTextField>
   </DesignSystemPanel>
 
@@ -173,20 +169,22 @@
       { value: "bristol", label: "Bristol", selected: true },
       { value: "oxford", label: "Oxford", disabled: true },
     ]}
-  />`}>
+  />`}
+  >
     <ONSSelect
       label="Select city"
       options={[
-      { value: "london", label: "London" },
-      { value: "bristol", label: "Bristol", selected: true },
-      { value: "oxford", label: "Oxford", disabled: true },
-    ]}
+        { value: "london", label: "London" },
+        { value: "bristol", label: "Bristol", selected: true },
+        { value: "oxford", label: "Oxford", disabled: true },
+      ]}
     />
   </DesignSystemPanel>
 
   <DesignSystemPanel
     title="Text area"
-    code={`<ONSTextArea id="text-area" hint="this is a hint for text area">Text area</ONSTextArea>`}>
+    code={`<ONSTextArea id="text-area" hint="this is a hint for text area">Text area</ONSTextArea>`}
+  >
     <ONSTextArea id="text-area" hint="this is a hint for text area">Text area</ONSTextArea>
   </DesignSystemPanel>
 
@@ -198,7 +196,8 @@
     <ONSRadio id="mixed">Mixed</ONSRadio>
     <ONSRadio id="white">White</ONSRadio>
     <ONSRadio id="other">Other</ONSRadio>
-  </ONSRadios>`}>
+  </ONSRadios>`}
+  >
     <ONSRadios name="ethnicity">
       <ONSRadio id="asian">Asian</ONSRadio>
       <ONSRadio id="black">Black</ONSRadio>
@@ -208,21 +207,15 @@
     </ONSRadios>
   </DesignSystemPanel>
 
-  <DesignSystemPanel
-    title="Accordion"
-    code={`<ONSAccordion {dataToConsume} />`}>
-    <ONSAccordion {dataToConsume}/>
+  <DesignSystemPanel title="Accordion" code={`<ONSAccordion {dataToConsume} />`}>
+    <ONSAccordion {dataToConsume} />
   </DesignSystemPanel>
 
-  <DesignSystemPanel
-    title="Category selector"
-    code={`<CategorySelector />`}>
+  <DesignSystemPanel title="Category selector" code={`<CategorySelector />`}>
     <CategorySelector />
   </DesignSystemPanel>
-  
-  <DesignSystemPanel
-    title="Share"
-    code={`<ONSShare url="https://www.google.com/">Share this page</ONSShare>`}>
+
+  <DesignSystemPanel title="Share" code={`<ONSShare url="https://www.google.com/">Share this page</ONSShare>`}>
     <ONSShare url="https://www.google.com/">Share this page</ONSShare>
   </DesignSystemPanel>
 
@@ -234,42 +227,44 @@
 </Topic>`}
   >
     <Topic topicList={topicList1} cardTitle="Health - Census 2021"
-    >The 2021 Census tells us a lot about the health of people living in England and Wales live and. <a href="#">
-      Choose a data option from the full list</a
-    > or explore one of these suggestions.
-    </Topic
-    >
+      >The 2021 Census tells us a lot about the health of people living in England and Wales live and. <a href="#">
+        Choose a data option from the full list</a
+      > or explore one of these suggestions.
+    </Topic>
   </DesignSystemPanel>
 
-  <DesignSystemPanel title="Topic Component-2" code={`<Topic topicList={topicList2} cardTitle="Need something specific from Census?">
+  <DesignSystemPanel
+    title="Topic Component-2"
+    code={`<Topic topicList={topicList2} cardTitle="Need something specific from Census?">
     Explore correlations between two indicators in <a href="#">advanced mode</a>.
-  </Topic>`}>
+  </Topic>`}
+  >
     <Topic topicList={topicList2} cardTitle="Need something specific from Census?"
-    >Explore correlations between two indicators in <a href="#">advanced mode</a>.
-    </Topic
-    >
+      >Explore correlations between two indicators in <a href="#">advanced mode</a>.
+    </Topic>
   </DesignSystemPanel>
 
-  <DesignSystemPanel title="Topic Component-3" code={`<Topic cardTitle="General health with other indicators"
+  <DesignSystemPanel
+    title="Topic Component-3"
+    code={`<Topic cardTitle="General health with other indicators"
     >Explore correlations between two indicators in <a href="#">advanced mode</a>.</Topic
-  >`}>
-
+  >`}
+  >
     <Topic cardTitle="General health with other indicators"
-    >Explore correlations between two indicators in <a href="#">advanced mode</a>.
-    </Topic
-    >
+      >Explore correlations between two indicators in <a href="#">advanced mode</a>.
+    </Topic>
   </DesignSystemPanel>
 
   <DesignSystemPanel title="Use Census Data" code={`<UseCensusData location="Crewe" />`}>
-    <UseCensusData/>
+    <UseCensusData />
   </DesignSystemPanel>
 
   <DesignSystemPanel title="Explore by Topic" code={`<ExploreByTopic />`}>
-    <ExploreByTopic/>
+    <ExploreByTopic />
   </DesignSystemPanel>
 
   <DesignSystemPanel title="Feedback" code={`<Feedback />`}>
-    <Feedback/>
+    <Feedback />
   </DesignSystemPanel>
 
   <DesignSystemPanel
@@ -280,11 +275,11 @@
   </DesignSystemPanel>
 
   <DesignSystemPanel title="Design system code block">
-    <DesignSystemCode code={`<DesignSystemCode code={\`<App />\`} />`}/>
+    <DesignSystemCode code={`<DesignSystemCode code={\`<App />\`} />`} />
   </DesignSystemPanel>
 </ONSCensusApp>
 
-<Feedback/>
+<Feedback />
 
 <style lang="scss">
   /* @import '../../node_modules/@ons/design-system/scss/main.scss';
