dc0d06bb5359a151c1bc516c98535643b0badd20
Michał Ordon <michal@tehidea.com>
2021-11-10 17:04:00 +0000

    Add Map

--- src/routes/Components.svelte
+++ src/routes/Components.svelte
@@ -1,10 +1,10 @@
 <script>
+  import Map from "../ui/Map.svelte";
   import TopicExplorer from "../ui/TopicExplorer.svelte";
   import BasePage from "../ui/BasePage.svelte";
   import DesignSystemPanel from "../ui/DesignSystemPanel.svelte";
   let serviceTitle = "Census Atlas components";
   let description = "Complex components made in Svelte with ONS Design System building blocks";
-
 </script>
 
 <svelte:head>
@@ -13,5 +13,6 @@
 
 <BasePage {serviceTitle} {description}>
   <h1>Census Atlas Components</h1>
+  <DesignSystemPanel title="Map placeholder" code={`<Map />`}><Map /></DesignSystemPanel>
   <DesignSystemPanel title="Topic explorer" code={`<TopicExplorer />`}><TopicExplorer /></DesignSystemPanel>
 </BasePage>
